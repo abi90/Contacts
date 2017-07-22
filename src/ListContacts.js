b9c5b94f2519d9a962ea4e347cc94e4c859445bc
abi90 <abisai.ramos@upr.edu>
2017-07-22 18:37:17 -0400

    Change ListContact implementation to a Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,33 @@
 import React, {Component} from 'react'
+/*Function Implementation (Stateless Functional Component)*/
+function ListContacts(props){
+  return (
+    <ol className='contact-list'>
+     {
+       props.contacts
+       .map(
+         (contact)=>(
+           <li key={contact.id} className="contact-list-item">
+             <div className="contact-avatar"
+               style={{
+                 backgroundImage: `url(${contact.avatarURL})`
+               }}/>
+             <div className='contact-details'>
+               <p>{contact.name}</p>
+               <p>{contact.email}</p>
+             </div>
+             <butoon className='contact-remove'>
+               Remove
+             </butoon>
+           </li>
+         )
+       )
+     }
+    </ol>
+  )
+}
 
+/*Class Implementation
 class  ListContacts extends Component {
   render() {
      return (
@@ -28,6 +56,6 @@ class  ListContacts extends Component {
    )
   }
 
-}
+}*/
 
 export default ListContacts
